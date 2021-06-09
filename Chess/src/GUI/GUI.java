74c664677fa1c5d0ce6868270efd9487857cc415
Zachary Gabriel <zachary.gabriel@student.adelaide.edu.au>
2021-06-09 19:24:18 +0930

    Preloaded images
    Improved performance by storing the images in an array so that they aren't loaded from memory every frame.

--- Chess/src/GUI/GUI.java
+++ Chess/src/GUI/GUI.java
@@ -8,7 +8,10 @@ import Pieces.*;
 // The 'graphical user interface' so the user caan play the game
 public class GUI extends PApplet
 {
+    // private variables
     Board board;
+    PImage[] images = new PImage[13]; // saves the images for faster performance
+
     // the main function that runs the settings(), then setup() then draw() functions.
     public static void main (String[] args) { PApplet.main(new String[]{"GUI.GUI"}); }
     
@@ -21,6 +24,9 @@ public class GUI extends PApplet
     // setup to the draw function
     public void setup() 
     {
+        // preloading images whilst runniong other instructions
+        preload_images();
+
         // makes the new board
         board = new Board();
         board.initial_position ();
@@ -34,9 +40,9 @@ public class GUI extends PApplet
     }
     
     // Executes this code once per frame
-    public void draw() 
+    public void draw () 
     {
-        
+        draw_board ();
     }
     
     void draw_board ()
@@ -49,77 +55,182 @@ public class GUI extends PApplet
             {
                 // tiles
                 if ((i+j) % 2 == 1)
-                    fill (92, 64, 51); // brown
+                    fill (92, 64, 51); // dark tiles
                 else
-                    fill ( 102, 51, 0); // grey
-                rect (i* (width/8), j* (height/8), (i+1)* (width/8), (j+1)* (height/8));
+                    fill (102, 51, 0); // light tiles
 
-                // blank space representing no piece
-                PImage img;
-                String name = "null";
+                // if hovering
+                if (i* (width /8) < mouseX && mouseX < (i+1)* (width /8) && 
+                    j* (height /8) < mouseY && mouseY < (j+1)* (height /8))
+                    fill (0); // black
+
+                // draws the rectangle
+                rect (i* (width /8), j* (height /8), (i+1)* (width /8), (j+1)* (height /8));
                 
-                //checking if a piece exists on said square
-                if (board.get_board()[j][i] != null) 
-                {
-                    // converting the piece into a letter
-                    switch (board.get_board()[j][i].get_piece())
-                    {
-                        case BISHOP:
-                            name = "Bishop";
-                            if (board.get_board()[j][i].get_side() == Side.BLACK)
-                                name += "_b";
-                            else
-                                name += "_w";
-                        break;
-                        
-                        case KING:
-                            name = "King";
-                            if (board.get_board()[j][i].get_side() == Side.BLACK)
-                                name += "_b";
-                            else
-                                name += "_w";
-                        break;
+                // if there's an image on the square, draw it
+                draw_image (i, j);
+            }
+        }
+    }
+    
+    void draw_image (int i, int j)
+    {
+        // the image name. 'null' represents nothing
+        String img_name = "null";
 
-                        case KNIGHT:
-                            name = "Knight";
-                            if (board.get_board()[j][i].get_side() == Side.BLACK)
-                                name += "_b";
-                            else
-                                name += "_w";
-                        break;
+        // checking if a piece exists on said square
+        if (board.get_board()[j][i] != null) 
+        {
+            // converting the piece into a letter
+            switch (board.get_board()[j][i].get_piece())
+            {
+                case BISHOP:
+                img_name = "Bishop";
+                if (board.get_board()[j][i].get_side() == Side.BLACK)
+                img_name += "_b";
+                else
+                img_name += "_w";
+                break;
+                
+                case KING:
+                img_name = "King";
+                if (board.get_board()[j][i].get_side() == Side.BLACK)
+                img_name += "_b";
+                else
+                img_name += "_w";
+                break;
+                
+                case KNIGHT:
+                img_name = "Knight";
+                if (board.get_board()[j][i].get_side() == Side.BLACK)
+                img_name += "_b";
+                else
+                img_name += "_w";
+                break;
+                
+                case PAWN:
+                img_name = "Pawn";
+                if (board.get_board()[j][i].get_side() == Side.BLACK)
+                img_name += "_b";
+                else
+                img_name += "_w";
+                break;
+                
+                case QUEEN:
+                img_name = "Queen";
+                if (board.get_board()[j][i].get_side() == Side.BLACK)
+                img_name += "_b";
+                else
+                img_name += "_w";
+                break;
+                
+                case ROOK:
+                img_name = "Rook";
+                if (board.get_board()[j][i].get_side() == Side.BLACK)
+                img_name += "_b";
+                else
+                img_name += "_w";
+            }
+            
+            int index = name_to_index (img_name);
+            if (index != -1)
+            {
+                image (images [index], i* (width/8), j* (height/8), (width/8) , (height/8));
+            }
+        }
+        return ;
+    }
+    
+    // Returns the index of the 'images array' where the image name is stored.
+    int name_to_index (String img_name)
+    {
+        switch (img_name)
+        {
+            case "null": 
+            return 0;
+            case "Bishop_b":
+            return 1;
+            case "Bishop_w": 
+            return 2;
+            case "King_w": 
+            return 3;
+            case "King_b": 
+            return 4;
+            case "Knight_w": 
+            return 5;
+            case "Knight_b": 
+            return 6;
+            case "Pawn_w": 
+            return 7;
+            case "Pawn_b": 
+            return 8;
+            case "Queen_w": 
+            return 9;
+            case "Queen_b": 
+            return 10;
+            case "Rook_w": 
+            return 11;
+            case "Rook_b": 
+            return 12;
+        }
+        return -1;
+    }
 
-                        case PAWN:
-                            name = "Pawn";
-                            if (board.get_board()[j][i].get_side() == Side.BLACK)
-                                name += "_b";
-                            else
-                                name += "_w";
-                        break;
+    // Stores the images in an array so that theyre only loaded once.
+    void preload_images ()
+    {
+        String img_name = "null.png";
+        PImage img = loadImage (img_name);
+        images[0] = img;
+        
+        img_name = "Bishop_b.png";
+        img = loadImage (img_name);
+        images[1] = img;
 
-                        case QUEEN:
-                            name = "Queen";
-                            if (board.get_board()[j][i].get_side() == Side.BLACK)
-                                name += "_b";
-                            else
-                                name += "_w";
-                        break;
+        img_name = "Bishop_w.png";
+        img = loadImage (img_name);
+        images[2] = img;
+        
+        img_name = "King_w.png";
+        img = loadImage (img_name);
+        images[3] = img;
 
-                        case ROOK:
-                            name = "Rook";
-                            if (board.get_board()[j][i].get_side() == Side.BLACK)
-                                name += "_b";
-                            else
-                                name += "_w";
-                        break;
+        img_name = "King_b.png";
+        img = loadImage (img_name);
+        images[4] = img;
+        
+        img_name = "Knight_w.png";
+        img = loadImage (img_name);
+        images[5] = img;
 
-                        default:
-                        name = "null";
-                    }
-                }
-                System.out.println(name);
-                img = loadImage(name);
-                image (img, i* (width/8), j* (height/8), (width/8) , (height/8));
-            } // end for i
-        } // end for j
-    } // end function
+        img_name = "Knight_b.png";
+        img = loadImage (img_name);
+        images[6] = img;
+        
+        img_name = "Pawn_w.png";
+        img = loadImage (img_name);
+        images[7] = img;
+
+        img_name = "Pawn_b.png";
+        img = loadImage (img_name);
+        images[8] = img;
+        
+        img_name = "Queen_w.png";
+        img = loadImage (img_name);
+        images[9] = img;
+
+        img_name = "Queen_b.png";
+        img = loadImage (img_name);
+        images[10] = img;
+        
+        img_name = "Rook_w.png";
+        img = loadImage (img_name);
+        images[11] = img;
+
+        img_name = "Rook_b.png";
+        img = loadImage (img_name);
+        images[12] = img;
+        
+        return ;
+    }
 }
